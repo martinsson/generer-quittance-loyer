5aacd93d4cbfe8514b530eaf9cdab07371c896dc
johan martinsson <jm1974@hotmail.com>
2020-01-06 14:47:20 +0100

    delete file after each test

--- test/generate_quittance.spec.js
+++ test/generate_quittance.spec.js
@@ -8,9 +8,16 @@ const {expect} = require("chai")
 const {google, drive_v3, docs_v1} = require('googleapis')
 
 describe("generate_quittance", () => {
+    let docId
+
+    afterEach(async function () {
+        await google.drive({version: "v3"}).files.delete({docId: docId, fileId:docId})
+
+    })
     it("generateQuittance", async () => {
         let destPath = path.join(os.tmpdir(), 'quittance.pdf')
-        await exportQuittance(destPath)
+        const doc = await exportQuittance(destPath)
+        docId = doc.data.id
         fs.existsSync(destPath)
         expect(fs.existsSync(destPath)).true
     }).timeout(10000)
